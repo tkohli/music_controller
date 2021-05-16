aeedf944aec3039ce41425705b95e71bdea5f1a5
Tanishka Kohli <tanishka.kohli00@gmail.com>
2021-05-16 20:47:38 +0530

    Day 1
    Room data api

---
+++ api/serializer.py
@@ -0,0 +1,10 @@
+# This take sour room and translates into a json file
+from rest_framework import serializers
+from .models import Room
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('id','code','host','guest_can_pause','votes_to_skip','created_at')
+
+
--- api/views.py
+++ api/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from rest_framework import generics
+from .serializer import RoomSerializer
+from .models import Room
 # Create your views here.
 
-def main(request):
-    return HttpResponse("<h1>Hello<h1>")
-
+#api view
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
