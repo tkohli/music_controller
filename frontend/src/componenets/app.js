d3b52f29b20737f8c6331cb810bdbd5d036fef5f
Tanishka Kohli <tanishka.kohli00@gmail.com>
2021-05-18 18:49:29 +0530

    Create app.js

---
+++ frontend/src/componenets/app.js
@@ -0,0 +1,12 @@
+import React,{Component} from "react";
+import { render } import "react-dom";
+
+export default class App extends Component {
+    constructor(props){
+        super(props);
+    }
+}
+
+render(){
+    return <h1> Testing React Code </h1>;
+}
